a3e61a9789fbcc10de7e3bea91315bdaa22c38d6
Trevor Mackin <tismetrm@gmail.com>
2020-02-04 17:28:53 -0800

    Update links in NavBar Build product boxes Seed database with additional products Add styling throughout Views

--- Ecommerce/wwwroot/js/site.js
+++ Ecommerce/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+console.log("Yay, I am writing in JavaScript again!!!");
+
+var theParent = document.querySelector(".parent-element");
+
+theParent.addEventListener("click", doSomething, false);
+ 
+function doSomething(event) {
+    if (event.target !== event.currentTarget) {
+        var clickedItem = event.target.id;
+        console.log("Hello " + clickedItem);
+        console.log(clickedItem);
+    }
+
+}
+
